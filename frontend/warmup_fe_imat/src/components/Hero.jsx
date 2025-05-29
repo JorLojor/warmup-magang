ae94f6478bcd349bdddc43d435c9f35cb0b982aa
AnnurImat91 <imat.10122293@mahasiswa.unikom.ac.id>
2025-05-29 22:44:04 +0700

    commit kedua update ke branch imat

--- frontend/warmup_fe_imat/src/components/Hero.jsx
+++ frontend/warmup_fe_imat/src/components/Hero.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <div className='bg-linear-to-r from-gray-900 to-gray-10 rounded-2xl px-9 py-24 mt-14 mx-10'>
-        <h1 className='text-5xl font-bold text-blue-50'>Selamat Datang Di Sewa Pesta Kita!</h1>
-        <p className='mt-5 text-blue-50'>Kami melayani berbagai layyanan sewa barang dan jasa di bidang penyedia alat event.</p>
-        <button className='mt-5  text-amber-50 border-2 px-6 py-4 rounded-4xl hover:bg-amber-50 transition-colors hover:text-black'>Order Sekarang</button>
+    <div className='bg-linear-to-r from-neutral-800 to-white rounded-2xl px-9 py-32 mt-14 mx-10'>
+        <h1 className='text-5xl font-bold text-neutral-50'>Selamat Datang Di Sewa Pesta Kita!</h1>
+        <p className='mt-5 text-slate-50'>Kami melayani berbagai layanan sewa barang dan jasa di bidang penyedia alat Event.</p>
+        <div className='flex flex-col gap-6 sm:flex-row'>
+          <button className='mt-5  text-amber-50 border-2 px-6 py-4 rounded-4xl hover:bg-amber-50 transition-colors hover:text-black'>Order Sekarang</button>
+          <button className='mt-5  text-amber-50 border-2 px-6 py-4 rounded-4xl hover:bg-amber-50 transition-colors hover:text-black'>Kontak</button>
+        </div>
     </div>
   )
 }
--- frontend/warmup_fe_imat/src/components/Navbar.jsx
+++ frontend/warmup_fe_imat/src/components/Navbar.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
+
+
 export const Navbar = () => {
+
   return (
-    <nav className='flex items-center justify-between border-b border-neutral-900'>
+    <nav className='relative flex items-center justify-between border-b border-neutral-900'>
         <div className='flex flex-shrink-0 items-center text-black text-3xl'>
             <a href="">LOGO</a>
         </div>
-        <div className='m-8 flex items-center justify-center gap-6 text-black text-xl'> 
+        <div className='my-5 hidden md:flex items-center justify-center gap-3 text-black text-2l'> 
             <a className='hover:bg-black px-5 py-1 rounded-2xl transition-colors hover:text-amber-50' href="">Home</a>
             <a className='hover:bg-black px-5 py-1 rounded-2xl transition-colors hover:text-amber-50' href="">Tentang Kami</a>
             <a className='hover:bg-black px-5 py-1 rounded-2xl transition-colors hover:text-amber-50' href="">Katalog Produk</a>
             <a className='hover:bg-black px-5 py-1 rounded-2xl transition-colors hover:text-amber-50' href="">Portfolio</a>
             <a className='hover:bg-black px-5 py-1 rounded-2xl transition-colors hover:text-amber-50' href="">Kontak</a>
         </div>
-        <div className='border-1 rounded-3xl'>
+        <div className='border-1 rounded-3xl md:block'>
             <input className='bg-gray-100 rounded-3xl text-black text-l p-3 pl-5' type="text" name="" id="" placeholder= 'Search' /> 
         </div>
     </nav>
